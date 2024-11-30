2495191d767cd838a540c5ae88404b11f509e4ec
Васин Юрий <ink2milk@gmail.com>
2024-11-30 22:26:56 +0300

    Create wiki-js.user.js

---
+++ wiki-js.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Wiki.js UI/UX enhancer
+// @namespace    https://github.com/ink-ru/wikijs-ui-tweaker
+// @version      0.1.1
+// @description  Wiki.js look & feel enhancer
+// @author       https://white-ink.space
+// @copyright    CopyLeft )
+// @homepage     https://white-ink.space
+// @match        https://wikijs.wb.ru/*
+// @icon         https://js.wiki/favicons/favicon-32x32.png
+// @icon64       https://js.wiki/favicons/apple-touch-icon.png
+// @grant       GM_addStyle
+// @grant       GM_setValue
+// @grant       GM_getValue
+// @grant       GM_registerMenuCommand
+// @grant       GM_unregisterMenuCommand
+// @run-at       document-end
+// @updateURL    https://raw.githubusercontent.com/ink-ru/wikijs-ui-tweaker/main/wiki-js.user.js
+// @downloadURL  https://raw.githubusercontent.com/ink-ru/wikijs-ui-tweaker/main/wiki-js.user.js
+// @supportURL   https://github.com/ink-ru/wikijs-ui-tweaker/issues
+// @tag UI/UX
+// @tag          wiki
+// @tag          productivity
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  //// @require      file:///home/georgew/tamper/wiki-js.user.js
+
+})();
