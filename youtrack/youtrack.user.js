a8c066db8e2847caec99ffcf994c26b69beecaa4
Васин Юрий <ink2milk@gmail.com>
2025-09-12 11:05:19 +0300

    Update youtrack.user.js

--- youtrack/youtrack.user.js
+++ youtrack/youtrack.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTrack UI/UX enhancer
 // @namespace    https://github.com/ink-ru/tampermonkey-ui-tweaker/tree/main/youtrack
-// @version      0.0.4
+// @version      0.0.5
 // @description  YouTrack look & feel enhancer
 // @author       https://white-ink.space
 // @copyright    CopyLeft )
@@ -22,7 +22,7 @@
 // @updateURL    https://github.com/ink-ru/tampermonkey-ui-tweaker/raw/refs/heads/main/youtrack/youtrack.user.js
 // @downloadURL  https://github.com/ink-ru/tampermonkey-ui-tweaker/raw/refs/heads/main/youtrack/youtrack.user.js
 // @supportURL   https://github.com//ink-ru/tampermonkey-ui-tweaker/issues
-// @resource     styles https://raw.githubusercontent.com/ink-ru/tampermonkey-ui-tweaker/refs/heads/main/youtrack/youtrack.user.css?v=0.0.4
+// @resource     styles https://raw.githubusercontent.com/ink-ru/tampermonkey-ui-tweaker/refs/heads/main/youtrack/youtrack.user.css?v=0.0.5
 // @tag         UI/UX
 // @tag         wiki
 // @tag         productivity
@@ -51,38 +51,40 @@
         });
     }
 
-    function clck_handler(wb_btn) {
-        GM_log('Button clicked!')
-        
-        document.querySelector("body").classList.toggle("custom_theme")
-
-        var panels_state = -1
-        panels_state = GM_getValue("panels_state", -1)
-    }
-
-    function wb_style_button(menu_bar)
+    function wb_buttons(elm)
     {
-        const css = GM_getResourceText("styles")
-        const button_html = '👀'
-        GM_addStyle(css)
+        let b_el = GM_addElement(elm, 'button', {
+            id: 'wb_ui_button',
+            class: 'button_a96a button_a96a heightS_efe7 buttonWithoutIcon_b3e8'
+        });
+
+        b_el.innerHTML += '👀'
         
-        waitForElm('.yt-agile-board__toolbar > ng-transclude').then((elm) => {
+        document.querySelector('#wb_ui_button').addEventListener("click", function (e) {
+            // wb_style_clck_handler(this)
+            document.querySelector("body").classList.toggle("custom_theme")
+        });
 
-                let b_el = GM_addElement(elm, 'button', {
-                  id: 'wb_ui_button',
-                  class: 'button_a96a button_a96a heightS_efe7 buttonWithoutIcon_b3e8'
-                });
+        // ========================
+        // @petrov.igor51
+        let petrov_el = GM_addElement(elm, 'button', {
+            id: 'petrov_ui_button',
+            title: '@petrov.igor51'
+            class: 'button_a96a button_a96a heightS_efe7 buttonWithoutIcon_b3e8'
+        });
 
-                b_el.innerHTML += button_html
+        petrov_el.innerHTML += 'Игорь'
 
-                document.querySelector('#wb_ui_button').addEventListener("click", function (e) {
-                    clck_handler(this)
-                });
-
-                GM_log('Button added!')
-            });
+        document.querySelector('#petrov_ui_button').addEventListener("click", function (e) {
+            document.querySelector("body").classList.toggle("show_petrov")
+        });
     }
 
-    wb_style_button();
+     waitForElm('.yt-agile-board__toolbar > ng-transclude').then((elm) => {
+         const css = GM_getResourceText("styles")
+         GM_addStyle(css)
+         
+         wb_buttons(elm)
+     });
 
 })();
